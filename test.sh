043e00bfff7a32010b680cacf112b0cca0bb94c0
yfs <stuyaofushang@163.com>
2019-08-10 19:09:56 +0800

    test

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/expect
+proc upload_github {path} {
+	puts $path
+	exec git add . 
+	exec git commit -m "test"
+#	exec git push -u origin master
+}
+upload_github /home/yfs/jupyter-dir/notebook
