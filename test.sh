af953f055524069b427bd60f7ec4a42d3dd59d68
yfs <stuyaofushang@163.com>
2019-08-10 19:17:05 +0800

    test

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@ proc upload_github {path} {
 	puts $path
 	exec git add . 
 	exec git commit -m "test"
-	
-	
-
 	exec git push origin master
+
+	expect "Username for 'https://github.com':"
+	send JeckFS
 
 }
 upload_github /home/yfs/jupyter-dir/notebook
