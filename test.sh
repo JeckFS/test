38be0e5c00ab4ae6516cda7610c30bbed1d41c11
yfs <stuyaofushang@163.com>
2019-08-10 19:10:10 +0800

    test

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ proc upload_github {path} {
 	puts $path
 	exec git add . 
 	exec git commit -m "test"
-#	exec git push -u origin master
+	exec git push -u origin master
 }
 upload_github /home/yfs/jupyter-dir/notebook
